57932878cd7e32ef1f4ce15213095612d370c4c3
Jair Junior <contato@jairjunior.eng.br>
2020-05-11 15:55:33 -0300

    Animation of the top navbar and now responsive from 320px media screen!

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
 "use strict";
+
+
+const navSlide = function(){
+     const burger =  document.querySelector('.burger-button');
+     const nav = document.querySelector('.navlinks');
+     const navLinks = document.querySelectorAll('.navlink');
+
+     // When click on the button burger
+     // Opem navbar with animation (slinding)
+     burger.addEventListener('click', () => {
+
+          nav.classList.toggle('nav-active');
+
+          // Navbar links animation
+          navLinks.forEach( (link, index) => {
+               if(link.style.animation)
+                    link.style.animation = '';
+               else
+                    link.style.animation = 'navLinksFade 0.6s ease forwards ' + (index/7 + 0.5) + 's';
+          });
+
+          // Change the button burger into a "X" for close navbar
+
+     })
+};
+
+navSlide();
